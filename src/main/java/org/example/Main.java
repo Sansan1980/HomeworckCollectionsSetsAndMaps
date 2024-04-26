7866572f07e799b266f0ca917f18b028b8e2636e
Homework-10 <danilovsansan@gmail.com>
2024-04-26 05:09:47 +0300

    задача 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,75 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(112, 111, 1, 1, 2, 3, 4, 4, 5, 6, 6, 7));
+        //Задача 1
+        printOddNambers(nums);
+
+        //Задача 2
+        Set<Integer> nums2 = new TreeSet<>(nums);
+        printEvenNumbers(nums2);
+        // Задача 4
+        List<String> strings = new ArrayList<>(List.of("два", "два", "четыре", "три", "три", "три", "один","четыре" , "четыре", "четыре"));
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String a : strings) {
+            if (map.containsKey(a)) {
+                for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                    // get key
+                    // String key = entry.getKey();
+                    // get value
+                    Integer v = entry.getValue();
+                 System.out.println(v++);
+                    map.put(a, v);
+                }
+                System.out.println(map + "||| " + a );
+            } else {
+                map.put(a, 1);
+               System.out.println(map + "ZZZ " + a);
+            }
+        }
     }
+
+
+
+/*
+
+        Set<String> k = map.keySet();
+        ArrayList<Integer> v = new ArrayList<>(map.values());
+        System.out.println(k);
+*/
+
+       /* for (Map.Entry<String, Integer> entry : strings) {
+
+            if (map.containsKey()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("Key: " + key + ", Value: " + value);
+            } else {
+                map.put(a, 1);
+                }*/
+//  System.out.println(v);
+
+
+    private static void printOddNambers(Collection<Integer> nambers) {
+        for (Integer a : nambers) {
+            if (a % 2 != 0) {
+                System.out.print(a + ", ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printEvenNumbers(Collection<Integer> nambers) {
+        for (Integer a : nambers) {
+            if (a % 2 == 0) {
+                System.out.print(a + ", ");
+            }
+        }
+        System.out.println();
+    }
+//-//-//-//
 }
