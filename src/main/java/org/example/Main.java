0c50b565dba399b8050f21c31e30b1dee423f13d
Homework-10 <danilovsansan@gmail.com>
2024-04-28 19:27:55 +0300

    задача 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,48 +4,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("//Задача 1");
         List<Integer> nums = new ArrayList<>(List.of(112, 111, 1, 1, 2, 3, 4, 4, 5, 6, 6, 7));
-        //Задача 1
         printOddNambers(nums);
 
-        //Задача 2
+        System.out.println("//Задача 2");
         Set<Integer> nums2 = new TreeSet<>(nums);
         printEvenNumbers(nums2);
-        // Задача 4
-        List<String> strings = new ArrayList<>(List.of("два", "два", "четыре", "три", "три", "три", "один","четыре" , "четыре", "четыре"));
-        Map<String, Integer> map = new HashMap<>();
 
-        for (String a : strings) {
-            if (map.containsKey(a)) {
-                map.merge(a, 1, Integer::sum);
-                System.out.println(map + "||| " + a );
-           } else {
-                map.put(a, 1);
-               System.out.println(map + "ZZZ " + a);
-            }
-        }
+        System.out.println("//Задача 3");
+        List<String> strings = new ArrayList<>(List.of("два", "два", "четыре", "три", "три", "три", "один", "четыре", "четыре", "четыре"));
+        uniqueValues(strings);
+
+        System.out.println("// Задача 4");
+        numberOfDuplicates(strings);
+
     }
-
-
-
-/*
-
-        Set<String> k = map.keySet();
-        ArrayList<Integer> v = new ArrayList<>(map.values());
-        System.out.println(k);
-*/
-
-       /* for (Map.Entry<String, Integer> entry : strings) {
-
-            if (map.containsKey()) {
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            System.out.println("Key: " + key + ", Value: " + value);
-            } else {
-                map.put(a, 1);
-                }*/
-//  System.out.println(v);
-
 
     private static void printOddNambers(Collection<Integer> nambers) {
         for (Integer a : nambers) {
@@ -64,5 +38,30 @@ public class Main {
         }
         System.out.println();
     }
+
+    private static void numberOfDuplicates(List<String> strings) {
+        Map<String, Integer> duplicate = new HashMap<>();
+        for (String a : strings) {
+            if (duplicate.containsKey(a)) {
+                duplicate.merge(a, 1, Integer::sum);
+            } else {
+                duplicate.put(a, 1);
+            }
+        }
+        System.out.println(duplicate.values());
+        System.out.println(duplicate);
+    }
+
+    private static void uniqueValues(List<String> strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String a : strings) {
+            if (map.containsKey(a)) {
+                ;
+            } else {
+                map.put(a, 1);
+            }
+        }
+        System.out.println(map.keySet());
 //-//-//-//
+    }
 }
